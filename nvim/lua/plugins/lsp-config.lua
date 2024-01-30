4b1f3a04465d65b3b170c150e194d06d04d5ae51
Christian Kaal <christiankaal@gmail.com>
2024-01-30 14:24:44 +0100

    Update lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,20 +1,21 @@
 return {
         {
                 "williamboman/mason.nvim",
+                lazy = false,
                 config = function()
                         require("mason").setup()
                 end
         },
         {
                 "williamboman/mason-lspconfig.nvim",
-                config = function()
-                        require("mason-lspconfig").setup({
-                                auto_install = true
-                        })
-                end
+                lazy = false,
+                opts = {
+                        auto_install = true,
+                },
         },
         {
                 "neovim/nvim-lspconfig",
+                lazy = false,
                 config = function()
                         local lspconfig = require("lspconfig")
                         lspconfig.lua_ls.setup({})
