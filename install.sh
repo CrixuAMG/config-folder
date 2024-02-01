0fc40a33b956921cbd03ca10545d085455911219
ckaal <c.kaal@visymo.com>
2024-02-01 16:23:38 +0100

    Add TPM

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 echo "Installing configurations..."
 
+echo "Installing TMUX plugin manager TPM"
+rm -rf ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+echo "Installed"
+
 echo "Copying TMUX configuration file"
 cp ./tmux/.tmux.conf ~/
 echo "Updated TMUX configuration"
