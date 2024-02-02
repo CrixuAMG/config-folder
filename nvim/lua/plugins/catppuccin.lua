1b4e80c55104f6441af52009e5b488d803eb9508
ckaal <c.kaal@visymo.com>
2024-02-02 12:27:54 +0100

    Add git blame, update catppuccin configuration

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,24 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+                require('catppuccin').setup({
+                        integrations = {
+                                cmp = true,
+                                gitsigns = true,
+                                nvimtree = true,
+                                treesitter = true,
+                                notify = true,
+                                markdown = true,
+                                dashboard = true,
+                                mini = {
+                                        enabled = true,
+                                        indentscope_color = "",
+                                },
+                        }
+                })
+
+                vim.cmd.colorscheme "catppuccin"
+        end
 }
