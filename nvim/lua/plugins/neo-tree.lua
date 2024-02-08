41c1b848d4b1f6df1b23f11e95456be732920a1c
ckaal <c.kaal@visymo.com>
2024-02-08 14:15:29 +0100

    WIP tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,9 +7,26 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {
+                    -- '.git',
+                    -- '.DS_Store',
+                    -- 'thumbs.db',
+                },
+                never_show = {},
+            },
+        }
+    },
     config = function()
         vim.keymap.set('n', '<leader>nt', ':Neotree filesystem toggle left <CR>', {})
         vim.keymap.set('n', '<leader>nf', ':Neotree filesystem focus left <CR>', {})
         vim.keymap.set('n', '<leader>nn', ':Neotree filesystem close <CR>', {})
+        vim.keymap.set('n', '<leader>nr', ':Neotree filesystem reveal left <CR>', {})
     end
 }
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,20 +1,20 @@
 return {
-        'nvim-tree/nvim-tree.lua',
-        lazy = true,
-        dependencies = {
-                'nvim-tree/nvim-web-devicons',
-        },
-        opts = {
-                filesystem = {
-                        filtered_items = {
-                                --visible = true,
-                                hide_dotfiles = false,
-                                hide_gitignored = true,
-                                hide_by_name = {
-                                        ".github",
-                                },
-                                never_show = { ".git" },
-                        },
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                --visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {
+                    ".github",
                 },
+                never_show = { ".git" },
+            },
         },
+    },
 }
