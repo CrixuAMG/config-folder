677ba9abdcea87b050bedf063837bb7d2743861f
ckaal <c.kaal@visymo.com>
2024-02-08 12:23:34 +0100

    Add Starship config to install script

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ echo "Installed"
 echo "Copying TMUX configuration file"
 cp $HOME/.config/tmux/.tmux.conf ~/
 echo "Updated TMUX configuration"
+
+echo "Copying Starship configuration file"
+cp $HOME/.config/starship.toml ~/
+echo "Updated Starship configuration"
