a923d203ebcd4ba88c600e5d561404bd2aac5a07
ckaal <c.kaal@visymo.com>
2024-02-13 09:30:25 +0100

    Update tmux, theme

--- nvim/lua/plugins/_theme-catppuccin.lua
+++ nvim/lua/plugins/_theme-catppuccin.lua
@@ -1,25 +1,24 @@
 return {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        enabled = false,
-        config = function()
-                require('catppuccin').setup({
-                        integrations = {
-                                cmp = true,
-                                gitsigns = true,
-                                nvimtree = true,
-                                treesitter = true,
-                                notify = true,
-                                markdown = true,
-                                dashboard = true,
-                                mini = {
-                                        enabled = true,
-                                        indentscope_color = "",
-                                },
-                        }
-                })
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require('catppuccin').setup({
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = true,
+                markdown = true,
+                dashboard = true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+            }
+        })
 
-                vim.cmd.colorscheme "catppuccin"
-        end
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
