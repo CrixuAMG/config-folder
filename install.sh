d3dcd7f2065a846d818375a44ef7d9c9d9557248
Christian Kaal <christiankaal@gmail.com>
2024-03-04 22:56:26 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+echo "Looking for brew..."
+if ! command -v brew &> /dev/null
+then
+    echo "Brew was not found, installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    exit 1
+else
+    echo "Found brew"
+fi
+
+brew install --cask kitty
+brew install zoxide atuin starship zsh man
+
 echo "Installing configurations..."
+
+echo "Copying .zshrc"
+cp $HOME/.config/.zshrc $HOME/.zshrc
+source $HOME/.zshrc
 
 echo "Copying Starship configuration file"
 cp $HOME/.config/starship.toml ~/
