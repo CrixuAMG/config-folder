1638164c3be2b4abc6598cf396f7624af39b23bf
Christian Kaal <christiankaal@gmail.com>
2024-03-04 22:48:50 +0100

    Remove tmux from install script

--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 
 echo "Installing configurations..."
 
-echo "Installing TMUX plugin manager TPM"
-rm -rf ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
-echo "Installed"
-
-echo "Copying TMUX configuration file"
-cp $HOME/.config/tmux/.tmux.conf ~/
-echo "Updated TMUX configuration"
-
 echo "Copying Starship configuration file"
 cp $HOME/.config/starship.toml ~/
 echo "Updated Starship configuration"
