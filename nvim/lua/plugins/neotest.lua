9c84e6106b1b130e771edb8fe80bfbed2250ff1b
ckaal <c.kaal@visymo.com>
2024-03-19 11:42:13 +0100

    Update luatest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,9 +1,21 @@
 return { 
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
-  }
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "olimorris/neotest-phpunit",
+    },
+    config = function ()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-phpunit")({
+                    phpunit_cmd = function()
+                        return "vendor/bin/phpunit"
+                    end
+                }),
+            }
+        })
+    end
 }
 
