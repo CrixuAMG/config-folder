b764599514cc2c8077edb36123f280691208f705
ckaal <c.kaal@visymo.com>
2024-03-21 09:07:51 +0100

    Remove dressing, add Harpoonline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,18 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
-        'nvim-tree/nvim-web-devicons'
+        'nvim-tree/nvim-web-devicons',
+        { "abeldekat/harpoonline", version = "*" },
     },
     config = function()
+        local Harpoonline = require("harpoonline").setup() -- using default config
+        local lualine_c = { Harpoonline.format, "filename" }
         require('lualine').setup({
             options = {
                 theme = 'dracula',
+            },
+            sections = {
+                lualine_c = lualine_c
             }
         })
     end
