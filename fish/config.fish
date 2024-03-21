1fbc35f38012d6b545268369b8bdb3ace2a6e058
ckaal <c.kaal@visymo.com>
2024-03-21 08:55:10 +0100

    Add vim alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,30 +1,32 @@
+alias vim "nvim"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
     starship init fish | source
     zoxide init fish --cmd=cd | source
 
-function bind_bang
+    function bind_bang
     switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
+    case "!"
+    commandline -t -- $history[1]
+    commandline -f repaint
+    case "*"
+    commandline -i !
     end
-end
+    end
 
-function bind_dollar
+    function bind_dollar
     switch (commandline -t)[-1]
-        case "!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
+    case "!"
+    commandline -f backward-delete-char history-token-search-backward
+    case "*"
+    commandline -i '$'
     end
-end
+    end
 
-function fish_user_key_bindings
+    function fish_user_key_bindings
     bind ! bind_bang
     bind '$' bind_dollar
-end
+    end
 end
