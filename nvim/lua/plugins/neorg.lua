69442966b4c365bab574dc1b8eef50485fc1779c
ckaal <c.kaal@visymo.com>
2024-03-26 12:29:06 +0100

    Update README, neorg

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -12,8 +12,16 @@ return  {
         config = function()
             require("neorg").setup {
                 load = {
-                    ["core.defaults"] = {}
-                }
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
             }
         end,
     }
