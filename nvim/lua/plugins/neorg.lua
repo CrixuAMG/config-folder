acd4c3cc07120fc1555b38cda5dd0a1ccf2ee293
ckaal <c.kaal@visymo.com>
2024-03-26 10:21:52 +0100

    Update neorg config

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,11 +1,20 @@
-return {
-    "nvim-neorg/neorg",
-    run = ":Neorg sync-parsers", -- This is the important bit!
-    config = function()
-        require("neorg").setup {
-            load = {
-                ["core.defaults"] = {}
+return  {
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+    },
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "luarocks.nvim" },
+        lazy = false, -- Disable lazy loading
+        run = ":Neorg sync-parsers", -- This is the important bit!
+        config = function()
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {}
+                }
             }
-        }
-    end,
+        end,
+    }
 }
