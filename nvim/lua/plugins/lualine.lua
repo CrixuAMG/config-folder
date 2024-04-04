72a11e93c44b56ecc85ccc1cf847dc674b8fecbb
ckaal <c.kaal@visymo.com>
2024-04-04 10:16:13 +0200

    Display lazy update status in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,10 +8,24 @@ return {
     config = function()
         -- local Harpoonline = require("harpoonline").setup() -- using default config
         -- local lualine_c = { Harpoonline.format, "filename" }
+        local lazy_status = require("lazy.status")
+
         require('lualine').setup({
             options = {
                 theme = 'dracula',
             },
+            sections = {
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates,
+                        color = { fg = "#ff9e64" }
+                    },
+                    { "encoding" },
+                    { "fileformat" },
+                    { "filetype" },
+                }
+            }
             -- sections = {
             --     lualine_c = lualine_c
             -- }
