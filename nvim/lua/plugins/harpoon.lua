5d4250e9e133f145f92fc477bd6b60d3a2ad6ce5
ckaal <c.kaal@visymo.com>
2024-04-08 08:38:00 +0200

    Update deps

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      {
+        "<leader>a",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():append()
+        end,
+        desc = "Add to Harpoon",
+      },
+      {
+        "<C-e>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Toggle Harpoon Quick Menu",
+      },
+      {
+        "<C-h>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():select(1)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<C-t>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():select(2)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<C-n>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():select(3)
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "<C-s>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():select(4)
+        end,
+        desc = "which_key_ignore",
+      },
+    },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
+  },
+}
+
