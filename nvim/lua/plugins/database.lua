20c119badb0f0b1e5f45169a47342b996d6ae360
ckaal <c.kaal@visymo.com>
2024-04-18 15:14:05 +0200

    Add keymap for dadbod-ui

--- nvim/lua/plugins/database.lua
+++ nvim/lua/plugins/database.lua
@@ -19,6 +19,8 @@ return {
         init = function()
             -- Your DBUI configuration
             vim.g.db_ui_use_nerd_fonts = 1
+
+            vim.keymap.set("n", '<leader>db', ':DBUI<CR>', { desc = 'Open Database' })
         end,
     },
     {
