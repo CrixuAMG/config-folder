11d06327c2da5247bb07f073799acb4f79497eb5
ckaal <c.kaal@visymo.com>
2024-04-29 10:57:46 +0200

    Add adr
    Add adr

--- nvim/lua/plugins/adr.lua
+++ nvim/lua/plugins/adr.lua
@@ -1,8 +1,10 @@
 return {
-    'dj95/adr-nvim',
+    'CrixuAMG/adr-nvim',
     opts = {},
     config = function()
-        require('adr').setup({})
+        require('adr').setup({
+            doc_dir = "/doc/adr",
+        })
 
         vim.keymap.set("n", "<leader>na", function() require('adr').create_from_template() end)
     end
