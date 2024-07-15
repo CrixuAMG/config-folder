c944dc61c976576405d643fb88c9de4bb15ae65f
ckaal <c.kaal@visymo.com>
2024-07-15 09:23:45 +0200

    Update fish config, update deps

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
-alias vim "nvim"
-alias ls "eza --long --git --header"
+alias vim nvim
+alias ls "eza --long --git --header --icons=always"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -11,20 +11,20 @@ if status is-interactive
 
     function bind_bang
         switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
+            case "!"
+                commandline -t -- $history[1]
+                commandline -f repaint
+            case "*"
+                commandline -i !
         end
     end
 
     function bind_dollar
         switch (commandline -t)[-1]
-        case "!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
+            case "!"
+                commandline -f backward-delete-char history-token-search-backward
+            case "*"
+                commandline -i '$'
         end
     end
 
@@ -33,4 +33,3 @@ if status is-interactive
         bind '$' bind_dollar
     end
 end
-
