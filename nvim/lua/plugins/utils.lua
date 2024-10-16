1b29f988cd0f4c5b9a20f5f99ffb806412a5da75
ckaal <c.kaal@visymo.com>
2024-10-16 09:10:12 +0200

    Update deps

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -6,5 +6,16 @@ return {
             let g:copypath_copy_to_unnamed_register = 1
             ]])
         end
+    },
+    {
+        "OXY2DEV/helpview.nvim",
+        lazy = false, -- Recommended
+
+        -- In case you still want to lazy load
+        -- ft = "help",
+
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter"
+        }
     }
 }
