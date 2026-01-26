f2792b17831432198aea72a7c99fdbe07b24b228
ckaal <c.kaal@visymo.com>
2026-01-26 11:06:05 +0100

    Add tiny code action

--- nvim/lua/plugins/bento.lua
+++ nvim/lua/plugins/bento.lua
@@ -1,5 +1,14 @@
 return {
-    "serhez/bento.nvim",
-    opts = {}
+    "CrixuAMG/bento.nvim",
+    config = function()
+        require("bento").setup({
+            ui = {
+                floating = {
+                    relative = "win", -- Position relative to active window instead of editor
+                    position = "middle-top", -- Position within the active pane
+                },
+            },
+        })
+    end,
 }
 
