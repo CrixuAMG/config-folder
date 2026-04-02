913c4c607b2e94e568ffd6bd258a84679be8bba2
ckaal <c.kaal@visymo.com>
2026-04-02 15:02:35 +0200

    WIP

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -11,17 +11,29 @@ return {
                 preset = 'default',
                 ['<C-k>'] = { 'snippet_forward', 'fallback' },
                 ['<C-j>'] = { 'snippet_backward', 'fallback' },
+                ['<Tab>'] = { 'select_next', 'fallback' },
+                ['<S-Tab>'] = { 'select_prev', 'fallback' },
+                ['<CR>'] = { 'accept', 'fallback' },
+                ['<Esc>'] = { 'cancel', 'fallback' },
+                ['<C-q>'] = { 'toggle_quick_menu', 'fallback' },
             },
             appearance = {
                 nerd_font_variant = 'mono',
+                use_nerd_fonts = true,
             },
             completion = {
                 documentation = {
                     auto_show = true,
+                    auto_show_delay_ms = 200,
+                },
+                menu_behavior = 'insert',
+                accept = {
+                    execute_on_select = false,
                 },
             },
             snippets = {
                 preset = 'luasnip',
+                expand = 'auto',
             },
             sources = {
                 default = {
@@ -30,10 +42,41 @@ return {
                     'snippets',
                     'buffer',
                 },
+                providers = {
+                    lsp = {
+                        score_offset = 15,
+                        fallback_for = { 'omni' },
+                    },
+                    path = {
+                        score_offset = 10,
+                    },
+                    buffer = {
+                        min_keyword_length = 3,
+                        max_items = 10,
+                    },
+                    snippets = {
+                        score_offset = -5,
+                    },
+                },
             },
             fuzzy = {
-                implementation = 'lua'
-            }
+                implementation = 'lua',
+            },
+            window = {
+                completion = {
+                    border = 'rounded',
+                    winhighlight = 'Normal:Normal,CursorLine:Visual,Search:None',
+                    col_offset = 0,
+                    side_padding = 1,
+                },
+                documentation = {
+                    border = 'rounded',
+                    winhighlight = 'Normal:Normal,CursorLine:Visual,Search:None',
+                },
+            },
+            cmdline = {
+                start_sources = { 'cmdline' },
+            },
         },
         opts_extend = {
             'sources.default'
