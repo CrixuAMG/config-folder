412c2d208f9c0daeb68be7387fd48adf07dd05e1
ckaal <c.kaal@visymo.com>
2026-04-21 16:04:40 +0200

    Add copy path

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -16,4 +16,23 @@ return {
             require("quicker").setup({})
         end,
     },
+    {
+        "hong4rc/copy-path.nvim",
+        event = "VeryLazy",
+        opts = {
+            keymaps = {
+                relative    = "<leader>fy",     -- src/foo/Bar.tsx
+                full        = "<leader>fY",     -- /home/user/project/src/foo/Bar.tsx
+                filename    = "<leader>fN",     -- Bar.tsx
+                line        = "<leader>fl",     -- src/foo/Bar.tsx:42
+                github_line = false,     -- https://github.com/.../Bar.tsx#L42
+                line_full   = false,            -- disabled by default
+                stem        = false,            -- disabled by default
+                extension   = false,            -- disabled by default
+                dir_full    = false,            -- disabled by default
+                dir_rel     = false,            -- disabled by default
+                github      = false,            -- disabled by default
+            },
+        },
+    },
 }
