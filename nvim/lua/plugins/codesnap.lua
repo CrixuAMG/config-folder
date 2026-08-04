a597cedb070d77487bda1b32cdd0d324c9f8b337
Christian Job Kaal <c.kaal@visymo.com>
2026-08-04 10:47:50 +0200

    Screenshot

--- nvim/lua/plugins/codesnap.lua
+++ nvim/lua/plugins/codesnap.lua
@@ -28,6 +28,42 @@ return {
             bg_theme = "peach"
         })
 
-       vim.keymap.set({"x","s"}, "<leader>ss", ":CodeSnap<cr>", { desc = "Take a Codesnap screenshot", silent=true })
+        local function copy_snapshot_to_kitty()
+            local path = "/tmp/codesnap.nvim.png"
+            local config = require("codesnap.config").get_config()
+            local generator = require("generator")
+
+            local ok, err = pcall(generator.save, path, config)
+            if not ok then
+                vim.notify("CodeSnap failed: " .. tostring(err), vim.log.levels.ERROR)
+                return
+            end
+
+            local encoded = vim.fn.system({ "base64", path }):gsub("%s", "")
+            if vim.v.shell_error ~= 0 then
+                vim.notify("Could not encode CodeSnap PNG", vim.log.levels.ERROR)
+                return
+            end
+
+            local mime = vim.base64.encode("image/png")
+            local packet = "\027]5522;type=write\027\\"
+            for index = 1, #encoded, 5460 do
+                packet = packet
+                    .. "\027]5522;type=wdata:mime="
+                    .. mime
+                    .. ";"
+                    .. encoded:sub(index, index + 5459)
+                    .. "\027\\"
+            end
+            packet = packet .. "\027]5522;type=wdata\027\\"
+            vim.api.nvim_ui_send(packet)
+            vim.cmd("delmarks <>")
+            vim.notify("CodeSnap screenshot copied to clipboard")
+        end
+
+        vim.keymap.set({ "x", "s" }, "<leader>ss", copy_snapshot_to_kitty, {
+            desc = "Take a Codesnap screenshot",
+            silent = true,
+        })
     end
 }
